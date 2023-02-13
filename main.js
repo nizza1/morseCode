ac00d52b44dac783968b46ce36f7d49267941626
Nizar Djassim <108374160+nizza1@users.noreply.github.com>
2023-02-13 12:23:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+
+// Hier hats nur als keys und values funktioniert 
+
+let morseAlphabet = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+
+  };
+
+const morseOutput = document.getElementById('morseOutput');
+
+function convertToMorseCode() {
+
+    let toMatchInPut = document.getElementById('inputUser').value.toUpperCase();
+
+    let output = '';
+    for (let i = 0; i < toMatchInPut.length; i++) {
+        let letter = toMatchInPut.charAt(i);
+
+        //hier vergleicht man jeder einzelne Zeichen
+        if(morseAlphabet[letter]){
+            output += morseAlphabet[letter]; 
+        } else {
+            output += "  ";
+        }
+    }
+    console.log(output);
+    morseOutput.innerHTML = output;
+}
